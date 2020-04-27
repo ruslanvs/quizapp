993daf59a9789373425616dad546d4ddf94bd179
Ruslan Suvorov <ruslanvs@yahoo.com>
2020-04-26 17:59:36 -0700

    globals topicsRef and reportRef

--- lib/services/globals.dart
+++ lib/services/globals.dart
@@ -1,6 +1,7 @@
 import 'services.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 
+// Static global state. Immutable services that do not care about build context.
 class Global {
   // App Data
   static final String title = 'Fireship';
@@ -15,5 +16,7 @@ class Global {
     Report: (data) => Report.fromMap(data),
   };
 
-
+  // Firestore References for Writes
+  static final Collection<Topic> topicsRef = Collection<Topic>(path: 'topics');
+  static final UserData<Report> reportRef = UserData<Report>(collection: 'reports');
 }
